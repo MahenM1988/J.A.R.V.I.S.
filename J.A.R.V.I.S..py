c45383ed65c473926cc5372c762e984b998b944b
MahenM1988 <mmahindaratne88@gmail.com>
2024-09-29 10:55:30 +0530

    Update J.A.R.V.I.S..py

--- J.A.R.V.I.S..py
+++ J.A.R.V.I.S..py
@@ -98,7 +98,7 @@ def play_file(file_path):
 
 # Main function
 def main():
-    api_key = "" # Replace with your OpenWeatherMap API Key
+    api_key = os.environ.get('WEATHER_API_KEY') # Replace with your OpenWeatherMap API Key
     use_console = input("Would you like to use the console (C) or speech recognition (S)? ").strip().upper()
 
     # Start the other scripts
